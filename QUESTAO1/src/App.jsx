b83240c334af617d856d2bc409586220319cfb9e
PP000777 <bia.silva007@hotmail.com>
2025-08-15 12:11:49 -0300

    first commit and... i do a first question

---
+++ QUESTAO1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Saudacao from "./components/Saudacao";
+
+function App() {
+  return (
+    <div>
+      <Saudacao nome="Pedro" />
+    </div>
+  );
+}
+
+export default App;
---
+++ QUESTAO1/src/components/Saudacao.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Saudacao(props) {
+    return (
+        <div>
+        <h1>Olá, {props.nome}!</h1>
+        </div>
+    );
+    }
+
+export default Saudacao;
